cf88cc8e28c723e79b8eb44c200d7f5c4ccf7f6f
Tawsifiit48 <tawsifrahman44@gmail.com>
2024-06-27 00:59:26 +0600

    added my running one

---
+++ main/Code Smell Detector/Java Module/CodeSmellDetector/src/main/java/com/CodeSmellDetector/CodeSmellDetectorApplication.java
@@ -0,0 +1,13 @@
+package com.CodeSmellDetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSmellDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeSmellDetectorApplication.class, args);
+	}
+
+}
